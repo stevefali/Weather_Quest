afa02f47fb53640a3754bd9d7669435ad42e1332
stevefali <stevefaliszewski@gmail.com>
2024-04-01 20:06:13 -0700

    Fix build.gradle and test log api key in MainActivity.kt

--- app/src/main/java/com/steve/weatherquest/MainActivity.kt
+++ app/src/main/java/com/steve/weatherquest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.IntentSender
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -313,7 +314,7 @@ fun WeatherDisplay(
                 isMenuShown = isMenuShown,
                 showMenu = { viewModel.showMenu() },
             )
-
+            Log.d(TAG, "TestApiKey: ${BuildConfig.HERE_API_KEY}")
         }
     }
 }
